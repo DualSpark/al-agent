9dded335805c7f6f5d72294edf99e5254edda331
john ramos <john.ramos@gmail.com>
2015-06-04 15:56:54 -0700

    changed default and broke out windows and _linux recipes

---
+++ recipes/windows.rb
@@ -0,0 +1,17 @@
+cache_dir = Chef::Config[:file_cache_path]
+basename = agent_file(node['al_agent']['package']['url'])
+cached_package = ::File.join(cache_dir, basename)
+
+remote_file basename do
+  path cached_package
+  source node['al_agent']['package']['url']
+  action :create_if_missing
+  notifies :install, "package[#{basename}]", :immediately
+end
+
+package basename do
+  source cached_package
+  action :nothing
+  options "#{windows_options}"
+end
+# reinstall causes an issue https://github.com/chef/chef/issues/3055s
