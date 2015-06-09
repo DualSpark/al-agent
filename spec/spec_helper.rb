887641fd03d4ae75fb04f0ff2e59eeeb8902e443
john ramos <john.ramos@gmail.com>
2015-06-09 10:53:38 -0700

    added the ALT_SEPARATOR fix for windows testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'fauxhai'
 
+# fixes a bug in chef-12.3.0/lib/chef/resource/windows_package.rb for testing windows on a linux platform
+# https://github.com/opscode-cookbooks/windows/issues/158
+::File::ALT_SEPARATOR = '/'
+
 ChefSpec::Coverage.start! do
   add_filter do |resource|
     resource.name =~ /log:/
@@ -28,9 +32,9 @@ CENTOS_OPTS = {
   :file_cache_path => '/tmp'
 }
 
-shared_context 'stubs' do
-  before do
-    allow_any_instance_of(Chef::Config).to receive(:file_cache_path)
-      .and_return('/tmp')
-  end
-end
+# shared_context 'stubs' do
+#   before do
+#     allow_any_instance_of(Chef::Config).to receive(:file_cache_path)
+#       .and_return('/tmp')
+#   end
+# end
