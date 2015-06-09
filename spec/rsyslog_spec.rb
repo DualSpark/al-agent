8be7682dbc3e28087326f185f8fe65eb7a110037
john ramos <john.ramos@gmail.com>
2015-06-09 11:53:13 -0700

    added rsyslog spec

---
+++ spec/rsyslog_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'al_agent::rsyslog' do
+  let(:chef_run) {
+    ChefSpec::SoloRunner.new(
+      platform: 'ubuntu',
+      version: '12.04'
+    ).converge(described_recipe)
+  }
+  let(:alertlogic_conf_file) { '/etc/rsyslog.d/alertlogic.conf' }
+
+  it 'creates an alertlogic.conf file' do
+    expect(chef_run).to render_file(alertlogic_conf_file)
+  end
+
+  it 'notifies the rsyslog service to restart' do
+    template = chef_run.template(alertlogic_conf_file)
+    expect(template).to notify('service[rsyslog]').to(:restart)
+  end
+
+end
