9d26bd628bc74dd2c4063de8dd165eac405d4413
john ramos <john.ramos@gmail.com>
2015-06-09 10:59:51 -0700

    cleanup on options not needed any longer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,30 +11,3 @@ ChefSpec::Coverage.start! do
     resource.name =~ /log:/
   end
 end
-
-LOG_LEVEL = :debug
-REDHAT_OPTS = {
-  :platform => 'redhat',
-  :version => '6.5',
-  :log_level => LOG_LEVEL,
-  :file_cache_path => '/tmp'
-}
-UBUNTU_OPTS = {
-  :platform => 'ubuntu',
-  :version => '14.04',
-  :log_level => LOG_LEVEL,
-  :file_cache_path => '/tmp'
-}
-CENTOS_OPTS = {
-  :platform => 'centos',
-  :version => '7.0',
-  :log_level => LOG_LEVEL,
-  :file_cache_path => '/tmp'
-}
-
-# shared_context 'stubs' do
-#   before do
-#     allow_any_instance_of(Chef::Config).to receive(:file_cache_path)
-#       .and_return('/tmp')
-#   end
-# end
