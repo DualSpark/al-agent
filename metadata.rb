0e9da4632218ecc1cc5b96c3e0ed41fdced0a3db
john ramos <john.ramos@gmail.com>
2015-06-17 17:32:22 -0700

    linting fixes to Thorfile, Vagrantfile and metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'al_agent'
-maintainer       'John Ramos DualSpark'
+name 'al_agent'
+maintainer 'John Ramos DualSpark'
 maintainer_email 'john.ramos@dualspark.com'
-license          'All rights reserved'
-description      'Installs/Configures the Alert Logic Agent'
+license 'All rights reserved'
+description 'Installs/Configures the Alert Logic Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version '0.1.0'
 
 depends 'selinux_policy'
 depends 'rsyslog'
